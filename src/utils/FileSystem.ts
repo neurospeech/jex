87dd22c98dc3ae5079d7163dcda741cfb0e96406
akashlocal <ackava@gmail.com>
2024-08-20 10:46:26 +0530

    Spawn fixed

--- src/utils/FileSystem.ts
+++ src/utils/FileSystem.ts
@@ -1,13 +1,25 @@
 import { copyFile, mkdir } from "fs/promises";
+import { homedir, userInfo } from "os";
+import { join } from "path";
 
 export const FileSystem = {
-    
+
+    get userHomeDir() {
+        return join(homedir(), userInfo().username);
+    },
+
+    expand(path: string) {
+        return path.startsWith("~")
+            ? `${this.userHomeDir}${path.substring(1)}`
+            : path;
+    },
+  
     Mkdir({ path }) {
-        return mkdir(path, { recursive: true });
+        return mkdir( this.expand(path), { recursive: true });
     },
 
     CopyFile({ src, dest }) {
-        return copyFile(src, dest);
+        return copyFile( this.expand(src), this.expand(dest));
     }
 
 };
