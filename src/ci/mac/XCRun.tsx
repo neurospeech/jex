2b4e743c87df489bb57666db81fc12583a0bd735
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-08-23 10:04:25 +0530

    XCRun updated

--- src/ci/mac/XCRun.tsx
+++ src/ci/mac/XCRun.tsx
@@ -1,10 +1,14 @@
+import { unlink, writeFile } from "fs/promises";
+import Batch from "../../core/Batch.js";
 import Run from "../../core/Run.js";
+import { FileSystem } from "../../utils/FileSystem.js";
 
 export interface IUploadApp {
     appPath: string;
     appType: "osx" | "ios" | "appletvos";
     apiKeyId: string;
     issuerId: string;
+    apiPrivateKey: string;
 }
 
 export const XCRun = {
@@ -12,21 +16,41 @@ export const XCRun = {
     UploadApp({
         appPath,
         appType,
+        apiPrivateKey,
         apiKeyId,
         issuerId,
     }: IUploadApp) {
-        return <Run
-            cmd="xcrun"
-            args={[
-                "altool",
-                "--output-format", "xml",
-                "--upload-app",
-                "--file", appPath,
-                "--type", appType,
-                "--apiKey", apiKeyId,
-                "--apiIssuer", issuerId
-            ]}
+
+        const apiKey = FileSystem.expand(`~/private_keys/AuthKey_${apiKeyId}.p8`);
+
+        const Install = async () => {
+            await writeFile(apiKey, apiPrivateKey, "utf8");
+        };
+
+        const Uninstall = async () => {
+            await unlink(apiKey);
+        };
+
+return  <Batch
+            cleanup={<Uninstall/>}
+            >
+
+            <Install
+                />
+
+            <Run
+                cmd="xcrun"
+                args={[
+                    "altool",
+                    "--output-format", "xml",
+                    "--upload-app",
+                    "--file", appPath,
+                    "--type", appType,
+                    "--apiKey", apiKeyId,
+                    "--apiIssuer", issuerId
+                ]}
             />
+        </Batch>;
     }
 
 };
