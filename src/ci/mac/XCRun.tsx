94790f4ac187e191774b9c563786af71d6646f76
akashlocal <ackava@gmail.com>
2024-08-23 15:40:35 +0530

    Added check

--- src/ci/mac/XCRun.tsx
+++ src/ci/mac/XCRun.tsx
@@ -1,7 +1,9 @@
-import { unlink, writeFile } from "fs/promises";
+import { mkdir, unlink, writeFile } from "fs/promises";
 import Batch from "../../core/Batch.js";
 import Run from "../../core/Run.js";
 import { FileSystem } from "../../utils/FileSystem.js";
+import { join } from "path";
+import { existsSync } from "fs";
 
 export interface IUploadApp {
     appPath: string;
@@ -21,9 +23,14 @@ export const XCRun = {
         issuerId,
     }: IUploadApp) {
 
-        const apiKey = FileSystem.expand(`~/private_keys/AuthKey_${apiKeyId}.p8`);
+        const privateKeys = join(process.env.HOME, "private_keys");
+
+        const apiKey =  join(privateKeys, `AuthKey_${apiKeyId}.p8`);
 
         const Install = async () => {
+            if (!existsSync(privateKeys)) {
+                await mkdir(privateKeys, { recursive: true });
+            }
             await writeFile(apiKey, apiPrivateKey, "utf8");
         };
 
