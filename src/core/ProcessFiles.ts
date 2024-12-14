381f1964377831028b6ddfb8cffb06eb1d8b6348
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-12-14 13:00:27 +0530

    Added support for dest folder

--- src/core/ProcessFiles.ts
+++ src/core/ProcessFiles.ts
@@ -5,14 +5,21 @@ import XNode from "./XNode.js";
 
 export default async function ProcessFiles({
     src,
+    dest,
     command,
     log = void 0,
     cleanup = void 0 as XNode
- }: { src: string, command: (file: string) => XNode | (() => XNode), cleanup?: XNode, log: any }) {
+ }: { src: string, dest: string, command: ({ file, dest }: { file: string, dest: string}) => XNode | (() => XNode), cleanup?: XNode, log: any }) {
 
     try {
+
+        let [_, root] = /^([^\*]+)/.exec(src);
+
         for await (const file of globIterate(src)) {
-            let element = await command(file) as any;
+
+            const destFile = dest + file.substring(root.length);
+
+            let element = await command({ file, dest: destFile }) as any;
             while (typeof element === "function") {
                 element = element();
             }
