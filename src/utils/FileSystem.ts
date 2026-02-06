455873b2fa2ed736dde22fdc35d707e12a34f49d
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-02-06 13:27:04 +0530

    added resolve back...

--- src/utils/FileSystem.ts
+++ src/utils/FileSystem.ts
@@ -45,7 +45,7 @@ export const FileSystem = {
         for (const file of files) {
             const srcPath = join(file.parentPath, file.name);
             const relativePath = relative( dirname(srcPath), src);
-            const destPath = join(dest, relativePath);
+            const destPath = resolve(dest, relativePath);
             console.log(`cp ${srcPath} ${destPath}`);
             await copyFile(srcPath, destPath);
         }
