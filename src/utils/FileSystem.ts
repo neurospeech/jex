9edfdad1188a84d7d0eb330ce46a714ab9fe5307
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-02-06 14:03:51 +0530

    wip

--- src/utils/FileSystem.ts
+++ src/utils/FileSystem.ts
@@ -45,7 +45,7 @@ export const FileSystem = {
         for (const file of files) {
             const srcPath = join(file.parentPath, file.name);
             const relativePath = srcPath.substring(src.length);
-            const destPath = join(dest, relativePath, file.name);
+            const destPath = resolve(join(dest, relativePath));
             console.log(`cp ${srcPath} ${destPath}`);
             await copyFile(srcPath, destPath);
         }
