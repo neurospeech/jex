3de93520694858ccade7781231853b0203b55f85
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-03-02 16:55:12 +0530

    pass hint removed

--- src/utils/Encryption.ts
+++ src/utils/Encryption.ts
@@ -65,7 +65,7 @@ export const Encryption = {
             passphrase
         }: IEncryptFileParams) {
 
-            console.log(`Decrypting ${input} to ${output} with ${Array.from(passphrase).map((v, i) => i === 0 ? v : "x").join("")}`)
+            console.log(`Decrypting ${input} to ${output}`)
 
             // --- Configuration ---
             const algorithm = 'aes-256-cbc';
